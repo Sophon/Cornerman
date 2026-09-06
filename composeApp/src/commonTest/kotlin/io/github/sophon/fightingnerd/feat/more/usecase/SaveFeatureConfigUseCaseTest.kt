8fb7dab69524da1de3b80b17691dbf5fd0d4abec
Le Duy Anh <lenduya@gmail.com>
2026-09-07 00:42:12 +0200

    test (more): rename test class

--- composeApp/src/commonTest/kotlin/io/github/sophon/fightingnerd/feat/more/usecase/SaveFeatureConfigUseCaseTest.kt
+++ composeApp/src/commonTest/kotlin/io/github/sophon/fightingnerd/feat/more/usecase/SaveFeatureConfigUseCaseTest.kt
@@ -22,7 +22,7 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 
-internal class SaveWikiConfigUseCaseTest {
+internal class SaveFeatureConfigUseCaseTest {
     @Test
     fun `usecase saves feature settings to the store`() = runTest {
         // given
