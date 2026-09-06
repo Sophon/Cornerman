5cfe01146b82e8eaa0ebcb8422b438f3957733e9
Le Duy Anh <lenduya@gmail.com>
2026-09-07 00:41:59 +0200

    test (more): exclude plain call usecase

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/more/usecase/SetUpdatePeriodUseCase.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/more/usecase/SetUpdatePeriodUseCase.kt
@@ -1,10 +1,12 @@
 package io.github.sophon.fightingnerd.feat.more.usecase
 
 import io.github.sophon.core.architecture.EmptyResult
+import io.github.sophon.core.architecture.ExcludeFromCoverage
 import io.github.sophon.fightingnerd.core.model.AppError
 import io.github.sophon.fightingnerd.feat.scheduler.Scheduler
 import kotlin.time.Duration
 
+@ExcludeFromCoverage("plain call")
 internal class SetUpdatePeriodUseCase(
     private val scheduler: Scheduler,
 ) {
