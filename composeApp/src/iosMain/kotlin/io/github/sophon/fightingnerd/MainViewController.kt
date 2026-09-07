7a3e179881bc54992a2718c33e54d371448d6ac3
Le Duy Anh <lenduya@gmail.com>
2026-09-07 03:16:04 +0200

    feat (scheduler): ios BGT stuff

--- composeApp/src/iosMain/kotlin/io/github/sophon/fightingnerd/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/io/github/sophon/fightingnerd/MainViewController.kt
@@ -1,20 +1,9 @@
 package io.github.sophon.fightingnerd
 
 import androidx.compose.ui.window.ComposeUIViewController
-import io.github.aakira.napier.DebugAntilog
-import io.github.aakira.napier.Napier
-import io.github.sophon.fightingnerd.feat.payment.initRevenueCat
-
-private var koinInitialized = false
 
 @Suppress("FunctionNaming")
 fun MainViewController() = ComposeUIViewController {
-    if (koinInitialized.not()) {
-        Napier.base(DebugAntilog())
-        initKoin()
-        initRevenueCat(BuildKonfig.REVENUECAT_API_KEY)
-        koinInitialized = true
-    }
-
+    iosAppInit()
     App()
 }
