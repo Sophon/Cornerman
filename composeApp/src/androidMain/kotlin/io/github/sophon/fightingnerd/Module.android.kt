344fa9946a2b87ad8dfe6384901beca7057f7d52
Le Duy Anh <lenduya@gmail.com>
2026-09-08 14:48:32 +0200

    feat (share): platform specific share sheet

--- composeApp/src/androidMain/kotlin/io/github/sophon/fightingnerd/Module.android.kt
+++ composeApp/src/androidMain/kotlin/io/github/sophon/fightingnerd/Module.android.kt
@@ -13,6 +13,8 @@ import io.github.sophon.fightingnerd.core.domain.UrlOpener
 import io.github.sophon.fightingnerd.core.domain.UrlOpenerAnd
 import io.github.sophon.fightingnerd.feat.scheduler.Scheduler
 import io.github.sophon.fightingnerd.feat.scheduler.WorkManagerScheduler
+import io.github.sophon.fightingnerd.feat.share.ShareSheet
+import io.github.sophon.fightingnerd.feat.share.ShareSheetAnd
 import io.github.sophon.fightingnerd.infrastructure.createDataStore
 import okio.Path
 import okio.Path.Companion.toOkioPath
@@ -27,6 +29,7 @@ internal actual val platformModule = module {
 
     singleOf(::UrlOpenerAnd).bind<UrlOpener>()
     singleOf(::WorkManagerScheduler).bind<Scheduler>()
+    singleOf(::ShareSheetAnd).bind<ShareSheet>()
 
     single<Path> { androidContext().filesDir.toOkioPath() / "media" }
 
---
+++ composeApp/src/iosMain/kotlin/io/github/sophon/fightingnerd/feat/share/ShareSheetIos.kt
@@ -0,0 +1,39 @@
+package io.github.sophon.fightingnerd.feat.share
+
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.usePinned
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import platform.Foundation.NSData
+import platform.Foundation.NSTemporaryDirectory
+import platform.Foundation.NSURL
+import platform.Foundation.dataWithBytes
+import platform.Foundation.writeToFile
+import platform.UIKit.UIActivityViewController
+import platform.UIKit.UIApplication
+
+@OptIn(ExperimentalForeignApi::class, BetaInteropApi::class)
+internal class ShareSheetIos : ShareSheet {
+
+    override suspend fun shareImage(pngBytes: ByteArray, fileName: String) {
+        val fileUrl = withContext(Dispatchers.IO) {
+            val path = NSTemporaryDirectory() + fileName
+            val nsData = pngBytes.usePinned { pinned ->
+                NSData.dataWithBytes(pinned.addressOf(0), pngBytes.size.toULong())
+            }
+            nsData.writeToFile(path, atomically = true)
+            val url = NSURL.fileURLWithPath(path)
+            url
+        }
+
+        val activityVc = UIActivityViewController(
+            activityItems = listOf(fileUrl),
+            applicationActivities = null,
+        )
+        UIApplication.sharedApplication.keyWindow
+            ?.rootViewController
+            ?.presentViewController(activityVc, animated = true, completion = null)
+    }
+}
