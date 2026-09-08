b1b1b7dcfec8a5e23fb9cfeb9e2362c1186c24d0
Le Duy Anh <lenduya@gmail.com>
2026-09-08 18:08:34 +0200

    refactor (changelog): rename changelog repo to release repo

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/core/data/ReleaseRepo.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/core/data/ReleaseRepo.kt
@@ -5,8 +5,8 @@ import io.github.sophon.core.architecture.EmptyResult
 import io.github.sophon.fightingnerd.feat.changelog.model.Release
 import kotlinx.coroutines.flow.Flow
 
-internal interface ChangelogRepo {
+internal interface ReleaseRepo {
     suspend fun saveLastSeenVersion(version: String): EmptyResult<DataError.Local>
-    fun getLastSeenVersion(): Flow<String>
+    fun getLastSeenVersion(): Flow<String?>
     fun getReleases(): Flow<List<Release>>
 }
--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/core/data/store/ReleaseRepoImpl.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/core/data/store/ReleaseRepoImpl.kt
@@ -8,7 +8,7 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import io.github.sophon.core.architecture.DataError
 import io.github.sophon.core.architecture.EmptyResult
 import io.github.sophon.core.architecture.Result
-import io.github.sophon.fightingnerd.core.data.ChangelogRepo
+import io.github.sophon.fightingnerd.core.data.ReleaseRepo
 import io.github.sophon.fightingnerd.feat.changelog.data.ChangelogRemoteSource
 import io.github.sophon.fightingnerd.feat.changelog.data.toDomain
 import io.github.sophon.fightingnerd.feat.changelog.model.Release
@@ -17,10 +17,10 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 
-internal class ChangelogRepoImpl(
+internal class ReleaseRepoImpl(
     private val store: DataStore<Preferences>,
     private val remoteSource: ChangelogRemoteSource,
-): ChangelogRepo {
+): ReleaseRepo {
     override suspend fun saveLastSeenVersion(version: String): EmptyResult<DataError.Local> {
         try {
             store.edit { prefs -> prefs[KEY_LAST_SEEN_VERSION] = version }
@@ -30,17 +30,16 @@ internal class ChangelogRepoImpl(
         }
     }
 
-    override fun getLastSeenVersion(): Flow<String> {
+    override fun getLastSeenVersion(): Flow<String?> {
         val lastSeenFlow = store.data
             .catch { emit(emptyPreferences()) }
-            .map { prefs -> prefs[KEY_LAST_SEEN_VERSION].orEmpty() }
+            .map { prefs -> prefs[KEY_LAST_SEEN_VERSION] }
         return lastSeenFlow
     }
 
     override fun getReleases(): Flow<List<Release>> {
         val releasesFlow = flow {
-            val releases = remoteSource.getReleaseNotes()
-                .map { it.toDomain() }
+            val releases = remoteSource.getReleaseNotes().toDomain()
             emit(releases)
         }
         return releasesFlow
